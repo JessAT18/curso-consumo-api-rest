5ef363ddaf66b38516ebc89273aef9d534ae97f8
JessAT18 <j-aquino@outlook.com>
2022-08-12 09:40:01 -0400

    Endpoint y query parameters

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 console.log('Holi');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
 async function reload() {
-    fetch(API_URL)
-    .then(response => response.json()) //Convertir respuesta a json
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });    
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
-fetch(API_URL)
-    .then(response => response.json()) //Convertir respuesta a json
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });
+reload();
