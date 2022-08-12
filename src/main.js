32c0b6ce17d25263e196223ddf8e8f239a3c5bc2
JessAT18 <j-aquino@outlook.com>
2022-08-12 11:33:26 -0400

    Website layout

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ async function reload() {
     const data = await response.json();
 
     console.log(data);
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
+    const img1 = document.getElementById('imgKitten1');
+    const img2 = document.getElementById('imgKitten2');
+    const img3 = document.getElementById('imgKitten3');
     
     img1.src = data[0].url;
     img2.src = data[1].url;
