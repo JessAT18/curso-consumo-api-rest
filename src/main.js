b07c8672f68cbe6bdadba7d09f11ac1502b9b61e
JessAT18 <j-aquino@outlook.com>
2022-08-12 12:03:42 -0400

    Reading favorite michis

--- src/main.js
+++ src/main.js
@@ -1,19 +1,37 @@
-console.log('Holi');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=b4da4f38-1d3b-497c-b1d0-e7905889d5f';
+const API_KEY = 'b4da4f38-1d3b-497c-b1d0-e7905889d5fa';
+const API_BASE_URL = 'https://api.thecatapi.com/v1'
+const API_RANDOM = `${API_BASE_URL}/images/search?limit=3&api_key=${API_KEY}`;
+const API_FAV = `${API_BASE_URL}/favourites/search?limit=3&api_key=${API_KEY}`;
 
-async function reload() {
-    const response = await fetch(API_URL);
+const spanError = document.getElementById('error');
+
+async function loadRandomMichis() {
+    const response = await fetch(API_RANDOM);
     const data = await response.json();
 
-    console.log(data);
-    const img1 = document.getElementById('imgKitten1');
-    const img2 = document.getElementById('imgKitten2');
-    const img3 = document.getElementById('imgKitten3');
-    
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    if (response.status !== 200){
+        spanError.innerHTML = `Hubo un error en random: ${response.status} ${data.message}`;
+    } else {
+        const img1 = document.getElementById('imgKitten1');
+        const img2 = document.getElementById('imgKitten2');
+        const img3 = document.getElementById('imgKitten3');
+        
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+        img3.src = data[2].url;
+    }
 }
 
-reload();
+async function loadFavoriteMichis() {
+    const response = await fetch(API_FAV);
+    const data = await response.json();
+    console.log(data);
+
+    if (response.status !== 200){
+        spanError.innerHTML = `Hubo un error en fav: ${response.status}`;
+    }
+}
+
+loadRandomMichis();
+loadFavoriteMichis();
